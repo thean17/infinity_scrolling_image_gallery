56c3b3a9b5e12f1767e2a595e55621e8a589638f
koonyew.thean <koonyew.thean@stengg.com>
2024-07-17 22:21:16 +0800

    add page and limit params to getImage

--- lib/utility/image_api.dart
+++ lib/utility/image_api.dart
@@ -3,14 +3,14 @@ import 'dart:convert';
 import 'package:http/http.dart';
 import 'package:infinity_scrolling_image_gallery/types/image.dart';
 
-class Api {
+class ImageApi {
   final Client client;
 
-  Api() : client = Client();
+  ImageApi() : client = Client();
 
-  Future<List<Image>> getImages() async {
-    final response =
-        await client.get(Uri.parse("https://picsum.photos/v2/list"));
+  Future<List<Image>> getImages({int page = 1, int limit = 30}) async {
+    final response = await client.get(
+        Uri.parse("https://picsum.photos/v2/list?page=${page}&limit=${limit}"));
 
     final decodedBody = jsonDecode(response.body) as List;
 
