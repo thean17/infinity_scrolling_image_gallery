738217d02abdd776009dd294c6c83fc0336121f4
koonyew.thean <koonyew.thean@stengg.com>
2024-07-20 00:26:52 +0800

    add method to download image as bytes from URL

--- lib/utility/image_api.dart
+++ lib/utility/image_api.dart
@@ -1,7 +1,20 @@
 import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:http/http.dart';
 import 'package:infinity_scrolling_image_gallery/types/image.dart';
+
+class AppException implements Exception {
+  final String message;
+
+  AppException(this.message);
+
+  @override
+  String toString() {
+    return message;
+  }
+}
 
 class ImageApi {
   final Client client;
@@ -9,11 +22,27 @@ class ImageApi {
   ImageApi() : client = Client();
 
   Future<List<Image>> getImages({int page = 1, int limit = 30}) async {
+    assert(page >= 1);
+
     final response = await client.get(
-        Uri.parse("https://picsum.photos/v2/list?page=${page}&limit=${limit}"));
+        Uri.parse("https://picsum.photos/v2/list?page=$page&limit=$limit"));
+
+    if (response.statusCode != HttpStatus.ok) {
+      throw AppException("HTTP response is not ok");
+    }
 
     final decodedBody = jsonDecode(response.body) as List;
 
     return decodedBody.map((entry) => Image.fromJson(entry)).toList();
+  }
+
+  Future<Uint8List> downloadImage(String url) async {
+    final response = await client.get(Uri.parse(url));
+
+    if (response.statusCode != HttpStatus.ok) {
+      throw AppException("HTTP response is not ok");
+    }
+
+    return response.bodyBytes;
   }
 }
